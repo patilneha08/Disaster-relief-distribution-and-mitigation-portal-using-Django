def7339b65dcfe745039166179076b0b359d8627
Neha Patil <patil.neha08@yahoo.com>
2021-06-01 16:08:29 +0530

    First Commit

---
+++ disastermanagement/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('index',views.index,name="index"),
+    path('about',views.about,name="about"),
+    path('floods',views.floods,name="floods"),
+    path('droughts',views.droughts,name="droughts"),
+    path('cyclones',views.cyclones,name="cyclones"),
+    path('earthquakes',views.earthquakes,name="earthquakes"),
+    path('covid',views.covid,name="covid"),
+    path('donate',views.donate,name="donate"),
+    path('volunteer',views.volunteer,name="volunteer"),
+    path('help',views.help,name="help"),
+    path('contact',views.contact,name="contact"),
+]
---
+++ disastermanagement/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'disastermanagement/index.html')
+
+def about(request):
+    return render(request,'disastermanagement/about.html')
+
+def floods(request):
+    return render(request,'disastermanagement/floods.html')
+
+def droughts(request):
+    return render(request,'disastermanagement/droughts.html')
+
+def cyclones(request):
+    return render(request,'disastermanagement/cyclones.html')
+
+def earthquakes(request):
+    return render(request,'disastermanagement/earthquakes.html')
+
+def covid(request):
+    return render(request,'disastermanagement/covid.html')
+
+def donate(request):
+    return render(request,'disastermanagement/donate.html')
+
+def volunteer(request):
+    return render(request,'disastermanagement/volunteer.html')
+
+def help(request):
+    return render(request,'disastermanagement/help.html')
+
+def contact(request):
+    return render(request,'disastermanagement/contact.html')
