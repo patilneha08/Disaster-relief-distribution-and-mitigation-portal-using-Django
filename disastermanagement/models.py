e9b97f0baedd1a5cccb8a56f340f348fc69f20c8
Neha Patil <patil.neha08@yahoo.com>
2021-06-08 18:01:11 +0530

    donate and contact page

--- disastermanagement/models.py
+++ disastermanagement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Donors(models.Model):
+    first_name=models.CharField(max_length=64, default="")
+    last_name=models.CharField(max_length=64, default="")
+    phone=models.BigIntegerField()
+    email=models.EmailField(max_length=64, default="")
+    amount=models.PositiveBigIntegerField
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}   {self.username}"
--- disastermanagement/views.py
+++ disastermanagement/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -24,6 +25,17 @@ def pandemic(request):
     return render(request,'disastermanagement/pandemic.html')
 
 def donate(request):
+    if request.method=='POST':
+        donation=Donors()
+        donation.first_name=request.POST.get('first_name')
+        donation.last_name=request.POST.get('last_name')
+        donation.email=request.POST.get('email')
+        donation.phone=request.POST.get('phone')
+        donation.amount=request.POST.get('amount')
+        return render(request,'disastermanagement/payment.html',{
+            "donation":donation
+        })
+        
     return render(request,'disastermanagement/donate.html')
 
 def volunteer(request):
@@ -34,3 +46,6 @@ def help(request):
 
 def contact(request):
     return render(request,'disastermanagement/contact.html')
+
+def payment(request):
+    pass
